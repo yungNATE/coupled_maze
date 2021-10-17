a944d160cbf8910788f4edd2dc91ffc9c8132bda
Nathan Martinigol <nmartinigol@iutbayonne.univ-pau.fr>
2021-10-17 18:09:43 +0200

    ajout d'une lib de parse JSOn + test lecture fichier

--- src/labyrinthe_PROJET/Main.java
+++ src/labyrinthe_PROJET/Main.java
@@ -1,10 +1,32 @@
 package labyrinthe_PROJET;
 
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// setup test
 		System.out.println("cc kiki");
+		
+		// lecture labyrinthe test
+		JSONParser parser = new JSONParser();
+	      try {
+	         Object obj = parser.parse(new FileReader("F://LP/JAVA/labyrinth/ressources/maps.json"));
+	         JSONObject jsonObject = (JSONObject)obj;
+	         JSONObject map = (JSONObject) jsonObject.get("map");    
+	         System.out.println(map);
+//	         Iterator iterator = subjects.iterator();
+//	         while (iterator.hasNext()) {
+//	            System.out.println(iterator.next());
+//	         }
+	      } catch(Exception e) {
+	         e.printStackTrace();
+	      }
 
 	}
 
