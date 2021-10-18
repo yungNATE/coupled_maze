f584ec6d4537558f12da9e8ecef85cf482455690
Chiara Gandolfi <cgandolfi@iutbayonne.univ-pau.fr>
2021-10-18 12:07:42 +0200

    start

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+
+import java.io.FileReader;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // setup test
+        System.out.println("cc kiki");
+
+        // lecture fichier test
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("ressources/maps.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONObject map = (JSONObject) jsonObject.get("map");
+            //System.out.println(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // lecture labyrinth test
+        JSONParser parser2 = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("ressources/maps.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONObject map = (JSONObject) jsonObject.get("map");
+            JSONArray premierNiveau = (JSONArray) map.get("premierNiveau");
+
+            Iterator iterator = premierNiveau.iterator();
+            while (iterator.hasNext()) {
+                System.out.println(iterator.next());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+
