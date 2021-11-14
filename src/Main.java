9b89fb625426528eefa7ec2b51942cd674429257
yungNATE <nathan.martinigol@gmail.com>
2021-11-14 22:54:39 +0100

    WIP

--- src/Main.java
+++ src/Main.java
@@ -88,7 +88,7 @@ public class Main {
                 title.setFont(new Font("Tahoma", Font.BOLD, 20));
                 title.setForeground(new Color(255,255,255));
 
-                JButton btn_return = new JButton("<-");
+                JButton btn_return = new JButton("Retour ...");
                 btn_return.setBackground(new Color(243,101,71));
                 btn_return.setForeground(new Color(255,255,255));
                 btn_return.setBorderPainted(false);
@@ -217,7 +217,7 @@ public class Main {
                 title.setFont(new Font("Tahoma", Font.BOLD, 20));
                 title.setForeground(new Color(255,255,255));
 
-                JButton btn_return = new JButton("<-");
+                JButton btn_return = new JButton("Retour...");
                 btn_return.setBackground(new Color(243,101,71));
                 btn_return.setForeground(new Color(255,255,255));
                 btn_return.setBorderPainted(false);
